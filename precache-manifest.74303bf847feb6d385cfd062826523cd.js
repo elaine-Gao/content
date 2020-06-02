16e99bc871d65371833df800e5487c6a366e0b41
Elaine Gao <gao.elaine164@gmail.com>
2020-06-02 20:49:08 +1000

    Updates

---
+++ precache-manifest.74303bf847feb6d385cfd062826523cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ca3aed748e39aa63b4a07dfa7544b1",
+    "url": "/content/index.html"
+  },
+  {
+    "revision": "4c79dcc68862de2a4290",
+    "url": "/content/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2b096e607a190a57a7f4",
+    "url": "/content/static/js/2.00054dfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/content/static/js/2.00054dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c79dcc68862de2a4290",
+    "url": "/content/static/js/main.cc756aed.chunk.js"
+  },
+  {
+    "revision": "e8f86bf515631f2be77f",
+    "url": "/content/static/js/runtime-main.7367b552.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/content/static/media/logo.5d5d9eef.svg"
+  }
+]);
